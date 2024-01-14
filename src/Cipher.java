30ba2050e98adaec274e4b83f01f9ac9be64cd46
sierranj03 <sierranj03@gmail.com>
2024-01-14 13:15:15 -0500

    implement caesar cipher

--- src/Cipher.java
+++ src/Cipher.java
@@ -1,3 +1,15 @@
 public class Cipher {
 
+    public static String caesar(String str, int key) {
+        StringBuilder output = new StringBuilder();
+        char[] chars = str.toCharArray();
+
+        for (char c : chars) {
+            c += key;
+            output.append(c);
+        }
+
+        return output.toString();
+    }
+
 }
--- src/Crypto.java
+++ src/Crypto.java
@@ -1,6 +1,6 @@
 public class Crypto {
     public static void main(String[] args) {
-        System.out.println(Encoder.hex(Encoding.OCTAL, "150 145 154 154 157 40 61 62 63 40 41 100 43"));
+        System.out.println(Cipher.caesar("djqifs", -1));
     }
 }
 
