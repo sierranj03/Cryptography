a10c63931f683f430031599c0d5fec49012e9505
sierranj03 <sierranj03@gmail.com>
2024-01-14 13:33:36 -0500

    implement xor cipher and update caesar cipher

--- src/Cipher.java
+++ src/Cipher.java
@@ -2,11 +2,19 @@ public class Cipher {
 
     public static String caesar(String str, int key) {
         StringBuilder output = new StringBuilder();
-        char[] chars = str.toCharArray();
 
-        for (char c : chars) {
-            c += key;
-            output.append(c);
+        for (int i = 0; i < str.length(); i++) {
+            output.append((char) (str.charAt(i) + key));
+        }
+
+        return output.toString();
+    }
+
+    public static String xor(String str, String key) {
+        StringBuilder output = new StringBuilder();
+
+        for (int i = 0; i < str.length(); i++) {
+            output.append((char) ((int) str.charAt(i) ^ (int) key.charAt(i % key.length())));
         }
 
         return output.toString();
--- src/Crypto.java
+++ src/Crypto.java
@@ -2,5 +2,6 @@ public class Crypto {
     public static void main(String[] args) {
         System.out.println(Cipher.caesar("djqifs", -1));
     }
+
 }
 
