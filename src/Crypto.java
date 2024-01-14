c4ce8b54ddda57157d674a38e815766a01d0cfe3
sierranj03 <sierranj03@gmail.com>
2024-01-14 13:02:30 -0500

    implement conversions for text, binary, octal, decimal, hex, base64, url

--- src/Crypto.java
+++ src/Crypto.java
@@ -1,6 +1,6 @@
 public class Crypto {
     public static void main(String[] args) {
-        System.out.println(Encoder.decimal(Encoding.DECIMAL, "105 32 108 111 118 101 32 121 111 117 10"));
+        System.out.println(Encoder.hex(Encoding.OCTAL, "150 145 154 154 157 40 61 62 63 40 41 100 43"));
     }
 }
 
--- src/Encoder.java
+++ src/Encoder.java
@@ -1,7 +1,6 @@
-import java.net.URL;
 import java.net.URLDecoder;
+import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
 import java.util.Base64;
 
 public class Encoder {
@@ -17,6 +16,10 @@ public class Encoder {
 
                 output.append((char) ascii);
             }
+        } else if (from != Encoding.TEXT) {
+            output.append(text(Encoding.DECIMAL, decimal(from, str)));
+        } else {
+            return str;
         }
 
         return output.toString();
@@ -33,6 +36,10 @@ public class Encoder {
 
                 output.append(String.format("%08d ", Integer.parseInt(binary)));
             }
+        } else if (from != Encoding.BINARY) {
+            output.append(binary(Encoding.DECIMAL, decimal(from, str)));
+        } else {
+            return str;
         }
 
         return output.toString();
@@ -49,6 +56,10 @@ public class Encoder {
 
                 output.append(String.format("%d ", Integer.parseInt(octal)));
             }
+        } else if (from != Encoding.OCTAL) {
+            output.append(octal(Encoding.DECIMAL, decimal(from, str)));
+        } else {
+            return str;
         }
 
         return output.toString();
@@ -58,64 +69,100 @@ public class Encoder {
         StringBuilder output = new StringBuilder();
 
         switch (from) {
-            case TEXT:
+            case TEXT -> {
                 char[] chars = str.toCharArray();
-
                 for (char c : chars) {
                     output.append(String.format("%d ", (int) c));
                 }
-                break;
-
-            case BINARY:
+            }
+            case BINARY -> {
                 String[] binaryValues = str.split(" ");
-
                 for (String binaryValue : binaryValues) {
                     int decimalValue = Integer.parseInt(binaryValue, 2);
 
                     output.append(String.format("%d ", decimalValue));
                 }
-                break;
-
-            case OCTAL:
+            }
+            case OCTAL -> {
                 String[] octalValues = str.split(" ");
-
                 for (String octalValue : octalValues) {
                     int decimalValue = Integer.parseInt(octalValue, 8);
 
                     output.append(String.format("%d ", decimalValue));
                 }
-                break;
-
-            case HEX:
+            }
+            case HEX -> {
                 String[] hexValues = str.split(" ");
-
                 for (String hexValue : hexValues) {
                     int decimalValue = Integer.parseInt(hexValue, 16);
 
                     output.append(String.format("%d ", decimalValue));
                 }
-                break;
-
-            case BASE64:
+            }
+            case BASE64 -> {
                 byte[] decimalValues = Base64.getDecoder().decode(str);
-
                 for (byte decimalValue : decimalValues) {
                     output.append(String.format("%d ", decimalValue));
                 }
-                break;
-
-            case URL:
+            }
+            case URL -> {
                 String urlText = URLDecoder.decode(str, StandardCharsets.UTF_8);
-
                 char[] urlChars = urlText.toCharArray();
-
                 for (char c : urlChars) {
                     output.append(String.format("%d ", (int) c));
                 }
-                break;
-
-            default:
+            }
+            default -> {
                 return str;
+            }
+        }
+
+        return output.toString();
+    }
+
+    public static String hex(Encoding from, String str) {
+        StringBuilder output = new StringBuilder();
+
+        if (from == Encoding.DECIMAL) {
+            String[] decimalValues = str.split(" ");
+
+            for (String decimalValue : decimalValues) {
+                String hex = Integer.toHexString(Integer.parseInt(decimalValue));
+
+                output.append(String.format("%s ", hex));
+            }
+        } else if (from != Encoding.HEX) {
+            output.append(hex(Encoding.DECIMAL, decimal(from, str)));
+        } else {
+            return str;
+        }
+
+        return output.toString();
+    }
+
+    public static String base64(Encoding from, String str) {
+        StringBuilder output = new StringBuilder();
+
+        if (from == Encoding.DECIMAL) {
+            output.append(Base64.getEncoder().encodeToString(text(Encoding.DECIMAL, str).getBytes()));
+        } else if (from != Encoding.BASE64) {
+            output.append(base64(Encoding.DECIMAL, decimal(from, str)));
+        } else {
+            return str;
+        }
+
+        return output.toString();
+    }
+
+    public static String url(Encoding from, String str) {
+        StringBuilder output = new StringBuilder();
+
+        if (from == Encoding.DECIMAL) {
+            output.append(URLEncoder.encode(text(Encoding.DECIMAL, str), StandardCharsets.UTF_8));
+        } else if (from != Encoding.URL){
+            output.append(url(Encoding.DECIMAL, decimal(from, str)));
+        } else {
+            return str;
         }
 
         return output.toString();
